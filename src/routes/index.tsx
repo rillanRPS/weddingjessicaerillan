c774c095ce8f035dfd442b70f6a4db7022a1d98e
rillanRPS <143342662+rillanRPS@users.noreply.github.com>
2026-09-13 19:27:49 -0300

    Add guestbook section to the page

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,6 +8,7 @@ import { Story } from "@/components/wedding/Story";
 import { Ceremony } from "@/components/wedding/Ceremony";
 import { Gallery } from "@/components/wedding/Gallery";
 import { Gifts } from "@/components/wedding/Gifts";
+import { Guestbook } from "@/components/wedding/Guestbook";
 import { Faq } from "@/components/wedding/Faq";
 import { Footer } from "@/components/wedding/Footer";
 
@@ -109,6 +110,7 @@ function Index() {
         <Ceremony />
         <Gallery />
         <Gifts />
+        <Guestbook />
         <Faq />
         <Footer />
       </main>
